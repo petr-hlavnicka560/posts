144e2403166a30725eacf032f5df37249562a93f
petr <peta.hlavnicku@gmail.com>
2023-01-12 16:05:11 +0100

    Import Awesome font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component'
 import { PostListComponent } from './components/post-list/post-list.component'
 import { HomeComponent } from './components/home/home.component'
 import { ListHeaderComponent } from './components/list-header/list-header.component'
+import { FontAwesomeModule } from '@fortawesome/fontawesome-svg-core'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ListHeaderComponent, PostListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
