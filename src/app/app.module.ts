f8f5362c2e1ad21b6c724924ab0bd828239b3ba2
petrhlavnicka <peta.hlavnicku@gmail.com>
2023-01-18 17:52:15 +0100

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component'
 import { PostListComponent } from './components/post-list/post-list.component'
 import { HomeComponent } from './components/home/home.component'
 import { ListHeaderComponent } from './components/list-header/list-header.component'
-import { FontAwesomeModule } from '@fortawesome/fontawesome-svg-core'
+import { HomeHeaderComponent } from './components/home-header/home-header.component'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ListHeaderComponent, PostListComponent],
+  declarations: [AppComponent, HomeComponent, ListHeaderComponent, PostListComponent, HomeHeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/home-header/home-header.component.ts
@@ -0,0 +1,12 @@
+/*
+ * Created by petrh on 18.01.2023
+ */
+
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-home-header',
+  templateUrl: './home-header.component.html',
+  styleUrls: ['./home-header.component.css'],
+})
+export class HomeHeaderComponent {}
