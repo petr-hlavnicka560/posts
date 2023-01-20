b2a29809e95a25d76eafd6e91d7027953562e93e
petrhlavnicka <peta.hlavnicku@gmail.com>
2023-01-20 22:04:18 +0100

    Add validation to Detail screen form

--- src/app/components/post-detail/detail-body/detail-body.component.ts
+++ src/app/components/post-detail/detail-body/detail-body.component.ts
@@ -3,7 +3,7 @@
  */
 
 import { Component } from '@angular/core'
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { ActivatedRoute } from '@angular/router'
 import { Store } from '@ngxs/store'
 import { DataState } from '../../../store/data-store'
@@ -16,26 +16,41 @@ import { UpdatePost } from '../../../store/data-actions'
   styleUrls: ['./detail-body.component.css'],
 })
 export class DetailBodyComponent {
-  myForm: FormGroup
   id: number
   post: Post | undefined
+  areButtonsFull = false
+  myForm: FormGroup
+  title: AbstractControl
+  body: AbstractControl
 
   constructor(private store: Store, private route: ActivatedRoute, fb: FormBuilder) {
     route.params.subscribe((params) => (this.id = Number(params['id'])))
     this.post = this.store.selectSnapshot(DataState.selectPosts).find((post) => post.id === this.id)
-    this.myForm = fb.group({ title: [this.post?.title], body: [this.post?.body] })
+    this.myForm = fb.group({
+      title: [this.post?.title, Validators.compose([Validators.required, titleLengthValidator])],
+      body: [this.post?.body, Validators.required],
+    })
+    this.title = this.myForm.controls['title']
+    this.body = this.myForm.controls['body']
   }
 
-  areButtonsFull = false
-
   onSubmit(value: { title: string; body: string }) {
-    this.store.dispatch(
-      new UpdatePost({
-        userId: this.post?.userId,
-        id: this.id,
-        title: value.title,
-        body: value.body,
-      })
-    )
+    if (value.title.length <= 200) {
+      this.store.dispatch(
+        new UpdatePost({
+          userId: this.post?.userId,
+          id: this.id,
+          title: value.title,
+          body: value.body,
+        })
+      )
+    }
+  }
+}
+
+// @ts-ignore
+function titleLengthValidator(control: FormControl): { [s: string]: boolean } {
+  if (control.value && control.value && control.value.length > 200) {
+    return { invalidTitleLength: true }
   }
 }
